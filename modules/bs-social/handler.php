46040c77f94d34520e22b041b8d3a08511a9740d
Arun Surath <asurath0@gmail.com>
2018-02-08 08:55:27 -0700

    Initial commit cleanup

--- modules/bs-social/handler.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once(  str_replace( "wp-content\plugins\agilepress\modules\bs-social" , "" , dirname(__FILE__)) . "wp-blog-header.php");
-$social = new SocialLoginPlugin();
-$social->Run();
